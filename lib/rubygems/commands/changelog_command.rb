acdaf1e38437c2868a400be5c9ff319b651b2e5b
Lars Henrik Mai <lars.mai@kontinui.de>
2011-06-17 08:11:35 +0200

    added gem command files

---
+++ lib/rubygems/commands/changelog_command.rb
@@ -0,0 +1,53 @@
+require 'rubygems/command'
+require 'rubygems/version_option'
+
+require 'changelog'
+
+#
+# gem command for displaying the changelog or history file of an installed gem
+#
+class Gem::Commands::ChangelogCommand < Gem::Command
+
+  include Gem::VersionOption
+
+  def initialize
+    super 'changelog',
+          'Display the changelog file of a gem',
+          :version => Gem::Requirement.default
+
+    add_version_option
+  end
+
+  def arguments # :nodoc:
+    'GEMNAME       name of an installed gem'
+  end
+
+  def defaults_str # :nodoc:
+    "--version='>= 0'"
+  end
+
+  def usage # :nodoc:
+    "#{program_name} GEMNAME [options]"
+  end
+
+  def execute
+    name = get_one_gem_name
+    spec = Gem::Specification.find_by_name name, options[:version]
+    changelog = spec.changelog
+
+    if changelog.missing?
+      say "No changelog file found for gem: #{spec.name} #{spec.version}"
+      terminate_interaction 1
+    elsif changelog.ambiguous?
+      say "Multiple possible changlog files found for gem: #{spec.name} #{spec.version}"
+      # TODO display candidates
+      terminate_interaction 1
+    else
+      say changelog.content
+    end
+
+  end
+
+
+end
+
